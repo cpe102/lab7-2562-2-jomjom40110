83b8435b7b07dd76e189dbd3892e706e40e31865
jomjom40110 <jomjom40110@gmail.com>
2020-01-21 16:30:07 +0700

    lab7

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -3,23 +3,33 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(float x){
+	char output;
+	if(x>90) output = 'A';
+	else if(x>75) output ='B';
+	else if(x>60) output ='C';
+	else if(x>45) output ='D';
+	else output ='F';
+	return output;
+}
 
 int main(){
 	//Input the number of students
 	int N,i = 0;
 	cout << "Enter the number of students: ";
 	cin >> N;
-	string name[N];
-	float score[N];	
+	string name[N],InName;
+	float score[N],InScore;	
 	
 	//Store names and scores of students into an array 
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,InName);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> InScore;
+		name[i]=InName;
+		score[i]=InScore;
 		i++;
 	}
 	
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,42 @@
-//status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	
+	int age,height,money;
+	string status;
+	
+	cout<<"Age : ";
+	cin>>age;
+	
+	if(age<=20)
+	{
+		cout<<"HEIGHT : ";
+		cin>>height;
+		if(height<160){
+			status ="UNFRIEND";
+			}else if(height<175){
+			status ="FRIEND";
+			}else{
+			cout<<"Money = ";
+			cin>>money;
+			if(money>69000000){
+				status ="MARRIED";		
+				}else{
+				status ="ONE-NIGHT-STAND";
+				}
+		}
+	}else if(age<=30){
+		cout<<"Money : ";
+		cin>>money;
+		if(money>10000000){
+			status ="BEST FRIEND";
+		}else{
+			status ="UNFRIEND";
+		}
+	}else{
+		status ="UNFRIEND";
+	}
+		cout <<status;
+}
